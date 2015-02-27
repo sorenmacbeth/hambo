a92f9241288069f00c6a5482fcee15c0e0b55156
Soren Macbeth <soren@dopeness.org>
2015-02-27 10:23:42 -0800

    repo stuff

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "https://github.com/yieldbot/hambo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"releases" {:url "s3p://maven.yieldbot.com/releases/"
+                             :username :env :passphrase :env}
+                 "snapshots" {:url "s3p://maven.yieldbot.com/snapshots/"
+                              :username :env :passphrase :env}}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [yieldbot/marceline "0.2.2-SNAPSHOT"]
                  [clojurewerkz/cassaforte "2.0.0"]]
-  :profiles {:provided {:dependencies [[org.apache.storm/storm-core "0.9.3"]]}})
+  :profiles {:provided {:dependencies [[org.apache.storm/storm-core "0.9.3"]]}}
+  :plugins [[s3-wagon-private "1.1.2"]])
